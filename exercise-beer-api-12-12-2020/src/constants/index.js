ac0fb7ef96ebfa104ce62b9b2162d9aa8fa208fb
ConradMeyer <meyeralten@gmail.com>
2021-03-22 09:23:32 +0100

    cleaning folders

--- exercise-beer-api-12-12-2020/src/constants/index.js
+++
@@ -1,5 +0,0 @@
-const SERVER_PORT = 3000
-
-module.exports = {
-  SERVER_PORT
-}
--- exercise-beer-api-12-12-2020/src/routes/beers.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// All your beers services go here...
-
-module.exports = router
